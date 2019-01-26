79cd28b6ff9292fd9efa366c5b23e03ae5a5a51d
王征 <1141502381@qq.com>
2019-01-26 09:17:43 +0800

    完成

--- app/src/main/java/flip/com/flipboradpractice/FlipBoardView.java
+++ app/src/main/java/flip/com/flipboradpractice/FlipBoardView.java
@@ -31,7 +31,7 @@ public class FlipBoardView extends View {
     //第三步抬起的角度
     private float degreeLast;
 
-    private Bitmap bitmap;
+    private Bitmap      bitmap;
     private AnimatorSet animatorSet;
 
 
@@ -69,16 +69,20 @@ public class FlipBoardView extends View {
 
         bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.flip_board);
 
+        //第一步抬起动画
         ObjectAnimator animator1 = ObjectAnimator.ofFloat(this, "degreeY", 0, -45);
         animator1.setDuration(1000);
 
-
+        //旋转动画
         ObjectAnimator animator2 = ObjectAnimator.ofFloat(this, "degreeZ", 0, -270);
         animator2.setDuration(1000);
 
+        //第三步抬起动画
+        ObjectAnimator animator3 = ObjectAnimator.ofFloat(this,"degreeLast",0,45);
+        animator3.setDuration(1000);
 
         animatorSet = new AnimatorSet();
-        animatorSet.playSequentially(animator1,animator2);
+        animatorSet.playSequentially(animator1, animator2,animator3);
         animatorSet.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
@@ -105,10 +109,12 @@ public class FlipBoardView extends View {
         //3. 旋转270度之后，抬起上部分。
 
         //实现思路：
+        //一、一半是翻起的状态
         //1. 用camera实现抬起右边，围着Y轴转-45度，就可以抬起
         //2. 最重要的一步，画抬起的一半，首先思路是这样的，就像移动坐标原点一样，这个抬起的状态旋转但是图片不旋转的效果也可以这么实现。
         //2.1 先让canvas围着Z轴，也就是平面旋转，然后旋转的时候把camera投影到canvas，然后顺便剪裁右边。因为旋转的是坐标轴，所以只剪裁右边就ok。
         //2.2 这时候不做操作那就是这个图转了，但是神奇的地方来了。这时候可以让canvas围着Z轴旋转回来。然后图片就没有旋转，但是抬起状态却保留下来了。
+        //二、另一半是不动的
         //3. 第三步画不动的一半。思路和第二步类似了。
 
         //注意：
@@ -151,12 +157,31 @@ public class FlipBoardView extends View {
         canvas.restore();
 
 
+        //画不动的一半
+        canvas.save();
+        mCamera.save();
+        canvas.translate(centerX, centerY);
+        //和变化的一半一样也是旋转，然后剪切另一半
+        canvas.rotate(degreeZ);
+        //剪切不变化的一半,此时坐标轴原点在centerX,centerY
+        canvas.clipRect(-centerX, -centerY, 0, centerY);
 
+        //最终抬起动作
+        //因为设置动画的时候已经是degreeZ为270,才会执行第三个抬起动作动画，所以这是的坐标系已经变化了，横着的就是Y轴
+        mCamera.rotateY(degreeLast);
+        mCamera.applyToCanvas(canvas);
+
+        canvas.rotate(-degreeZ);
+        canvas.translate(-centerX, -centerY);
+
+        canvas.drawBitmap(bitmap, bpLeft, bpTop, paint);
+        mCamera.restore();
+        canvas.restore();
 
     }
 
 
-    public void reset(){
+    public void reset() {
         degreeLast = 0;
         degreeY = 0;
         degreeZ = 0;
